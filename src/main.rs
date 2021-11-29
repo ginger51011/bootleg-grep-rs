e02046837b40389a19867e9fb33edf9b1edff8f2
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-29 13:23:05 +0100

    Added test data, changed read_lines a bit

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let pattern = Regex::new(&args[0]).unwrap();
-    let path = Path::new(&args[1]);
+
+    // args[0] is name of program
+    let pattern = Regex::new(&args[1]).unwrap();
+    let path = Path::new(&args[2]);
 
     // File read properly
     if let Ok(lines) = read_lines(path) {
